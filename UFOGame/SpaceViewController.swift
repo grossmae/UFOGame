e9e0204b981b774322649133ebd58533aeac30fc
Evan Grossman <grossmae@gmail.com>
2016-04-06 23:13:16 -0700

    Renamed ViewController to SpaceViewController

--- UFOGame/SpaceViewController.swift
+++ UFOGame/SpaceViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SpaceViewController.swift
 //  UFOGame
 //
 //  Created by Evan Grossman on 4/6/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SpaceViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
