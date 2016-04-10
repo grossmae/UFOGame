11b328869cec14441b5382f1488484c148085af7
Evan Grossman <grossmae@gmail.com>
2016-04-09 23:47:07 -0700

    Added planet rotation with pan gestures

--- UFOGame/SpaceViewController.swift
+++ UFOGame/SpaceViewController.swift
@@ -14,11 +14,17 @@ class SpaceViewController: UIViewController {
     var scnView: SCNView!
     var spaceScene: SCNScene!
     
+    var ufoNode: SCNNode!
+    var planetNode: SCNNode!
+    var currentPlanetXAngle: Float = 0.0
+    var currentPlanetZAngle: Float = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.redColor()
         setupScenes()
+        setupNodes()
+        setupGestures()
     }
     
     func setupScenes() {
@@ -27,7 +33,35 @@ class SpaceViewController: UIViewController {
         spaceScene = SCNScene(named: "/UFOGame.scnassets/Space.scn")
         scnView.scene = spaceScene
     }
+    
+    func setupNodes() {
+        ufoNode = spaceScene.rootNode.childNodeWithName("UFO", recursively: true)!
+        planetNode = spaceScene.rootNode.childNodeWithName("Earth", recursively: true)!
+    }
+    
+    func setupGestures() {
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture))
+        scnView.addGestureRecognizer(panGestureRecognizer)
+    }
 
+    func handlePanGesture(sender: UIPanGestureRecognizer) {
+        let translation = sender.translationInView(sender.view!)
+        
+        var newZAngle = (Float)(-translation.x)*(Float)(M_PI)/360.0
+        newZAngle += currentPlanetZAngle
+        var newXAngle = (Float)(translation.y)*(Float)(M_PI)/360.0
+        newXAngle += currentPlanetXAngle
+        
+        let zRotation = SCNMatrix4MakeRotation(newZAngle, 0, 0, 1)
+        let xRotation = SCNMatrix4MakeRotation(newXAngle, 1, 0, 0)
+        planetNode.transform = SCNMatrix4Mult(zRotation, xRotation)
+        
+        if(sender.state == UIGestureRecognizerState.Ended) {
+            currentPlanetXAngle = newXAngle
+            currentPlanetZAngle = newZAngle
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
